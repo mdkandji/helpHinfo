030bfe9b3c095aa2f4eb24967cd86f7db04c00d2
Diarra <mkandji.ledantec@gmail.com>
2024-10-09 15:23:12 +0000

    ajout évaluation mémoire libre

--- helpInfo.sh
+++ helpInfo.sh
@@ -2,3 +2,8 @@
 #### Linux Version
 echo "#### Linux Version"
 lsb_release -d | cut -f2- || cat /etc/os-release | grep PRETTY_NAME | cut -d '"' -f2
+
+
+#### Free Memory
+echo -e "\n#### Free Memory"
+free -h | grep "Mem:" | awk '{print $4}'
